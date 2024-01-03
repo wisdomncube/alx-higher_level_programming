8d45d229b0b5630fc39665954f21d6d18e00daee
wisdomncube <wizzsiyancube@icloud.com>
2024-01-03 07:40:02 -0800

    Task 0

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+	print("Number:", number)
+
+	if number > 0:
+	print("is positive")
+	elif:
+	print("is zero")
+	else:
+	print ("is negative")
