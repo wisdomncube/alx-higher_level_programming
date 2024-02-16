cdf2f6ca3314df4e54ddaf377741a69c7b9757f4
wisdomncube <wizzsiyancube@icloud.com>
2024-02-15 23:45:51 -0800

    Refactoring code

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -16,4 +16,4 @@ def append_write(filename="", text=""):
         A file with appened text
     """
     with open(filename, 'a', encoding='utf-8') as file:
-        return file.write(text)i
+        return file.write(text)
